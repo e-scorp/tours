ccfced1144d587ed88be6a55c53832209e9cf800
YashwantNG <yasgavande78@gmail.com>
2023-01-05 19:15:28 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-  base:'/tour/',
-=======
-  base:'/tours/,
->>>>>>> 785112be531c32f122b146c441a43070364430fd
+  base:'/tours/',
   plugins: [react()],
 })
